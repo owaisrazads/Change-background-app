2d86b651ef37f7e723e8c3c56052c0591dbe3472
unknown <oraza7969@gmail.com>
2023-09-21 09:59:18 -0700

    Bg-color change

---
+++ app.js
@@ -0,0 +1,10 @@
+// const ChangeBgColor = document.querySelector('button')
+// console.log(ChangeBgColor);
+const p = document.querySelector('p');
+
+function ChangeBackgroundColor(color){
+    document.body.style.backgroundColor = color;
+}
+function para(p){
+    p.innerHTML += color;
+}
